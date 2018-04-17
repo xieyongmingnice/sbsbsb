157db1b08f12362686c7bd3e0a48873372e504c6
dingtonghao <13520279294@163.com>
2018-04-17 17:38:11 +0800

    修改名称

--- xunyu_eureka/src/main/java/com/xunyu/eureka/EurekaServer.java
+++ xunyu_eureka/src/main/java/com/xunyu/eureka/EurekaServer.java
@@ -6,16 +6,17 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * @author dth
+ * @date 2018/4/17 17:34
  * spring cloud 集成kafka
  * https://blog.csdn.net/ldy1016/article/details/72852179
  */
 
 @EnableEurekaServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-public class ServerEurekaStarter {
-    public static void main(String[] args) {
-        SpringApplication.run(ServerEurekaStarter.class, args);
-
-        System.out.println("启动discovery ServerDiscoveryStarter 成功！");
-    }
+public class EurekaServer {
+        public static void main(String[] args) {
+            SpringApplication.run(EurekaServer.class, args);
+            System.out.println("启动discovery ServerDiscoveryStarter 成功！");
+        }
 }
