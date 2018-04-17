456efef99a71b4db4b219a00445c59dc001b9aab
dingtonghao <13520279294@163.com>
2018-04-17 17:49:46 +0800

    增加eureka注册中心

---
+++ xunyu-eureka/src/main/java/com/xunyu/xunyueureka/XunyuEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.xunyu.xunyueureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XunyuEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XunyuEurekaApplication.class, args);
+    }
+}
---
+++ xunyu-eureka/src/test/java/com/xunyu/xunyueureka/XunyuEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xunyu.xunyueureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XunyuEurekaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
