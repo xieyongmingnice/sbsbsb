9c8acc7382c77a36ade36cfe03889768405fe313
dingtonghao <13520279294@163.com>
2018-04-17 18:02:26 +0800

    增加日志

--- xunyu-eureka/src/main/java/com/xunyu/xunyueureka/XunyuEurekaApplication.java
+++ xunyu-eureka/src/main/java/com/xunyu/xunyueureka/XunyuEurekaApplication.java
@@ -1,12 +1,19 @@
 package com.xunyu.xunyueureka;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
+@EnableEurekaServer
 @SpringBootApplication
 public class XunyuEurekaApplication {
-
+    private static final Logger log = LoggerFactory.getLogger(XunyuEurekaApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(XunyuEurekaApplication.class, args);
+        log.info("Eureka服务启动成功...");
     }
 }
