1fb3de13ed434eb77fd700a1b4e2d7dbac5507e9
dingtonghao <13520279294@163.com>
2018-04-26 09:13:17 +0800

    增加图片验证工具类

---
+++ xunyu_config/src/main/java/com/xunyu/config/oos/ImgUtils.java
@@ -0,0 +1,27 @@
+package com.xunyu.config.oos;
+
+import java.util.List;
+
+/**
+ * @author dth
+ * @date 2018/4/26 9:12
+ **/
+public class ImgUtils {
+
+    // 对图片格式进行判断
+    public static boolean isImg(String extend) {
+        boolean ret = false;
+        List<String> list = new java.util.ArrayList<String>();
+        list.add("jpg");
+        list.add("jpeg");
+        list.add("bmp");
+        list.add("gif");
+        list.add("png");
+        list.add("tif");
+        for (String s : list) {
+            if (s.equalsIgnoreCase(extend))
+                ret = true;
+        }
+        return ret;
+    }
+}
