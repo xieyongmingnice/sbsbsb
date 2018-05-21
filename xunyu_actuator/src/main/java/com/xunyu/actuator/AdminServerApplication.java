e30d350093fc6b2b568f28cbdcc2e9d3b61931d6
dingtonghao <13520279294@163.com>
2018-05-21 10:43:54 +0800

    微服务的系统监控可视化管理

---
+++ xunyu_actuator/src/main/java/com/xunyu/actuator/AdminServerApplication.java
@@ -0,0 +1,25 @@
+package com.xunyu.actuator;
+
+/**
+ * @author dth
+ * @date 2018/5/21 9:07
+ **/
+
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 监控系统运行情况
+ */
+@EnableAdminServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
